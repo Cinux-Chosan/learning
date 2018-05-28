3d7b75b31a4660186d18bfecee206e6495c01e56
Chosan <179817004@qq.com>
2018-05-29 00:04:12 +0800

    双色球

--- Python-learning/test.py
+++ Python-learning/test.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python 
 #coding:utf-8
-
-import requests
-
-r = requests.get("https://chosan.cn:3000/api/posts")
-
-print(r.text)
+from random import random 
+from math import ceil, floor
+firstNums = list(range(1, 34))
+all = []
+while len(all) < 6:
+    all.append(firstNums.pop(floor(random() * len(firstNums))))
+all.sort()
+all.append(ceil(random() * 33))
+','.join([str(x) for x in all])
+print(all)
