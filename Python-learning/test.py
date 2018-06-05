6a423506483fc5489d40118ccf3247ed0ca5cd74
chosan <179817004@qq.com>
2018-06-05 20:27:16 +0800

    ..

--- Python-learning/test.py
+++ Python-learning/test.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python
-#coding:utf8
-
-print('adsfa')
+#coding:utf-8
+from random import random
+from math import floor
+firstNums = list(range(1, 34))
+all = [firstNums.pop(floor(random() * len(firstNums))) for tmp in range(6)]
+all.sort()
+all.append(1 + floor(random() * 16))
+print(all)
