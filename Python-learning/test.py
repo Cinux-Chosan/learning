07df0fb21e784956db15c0b1771e726774bfeed1
chosan <179817004@qq.com>
2018-06-07 20:33:59 +0800

    python

--- Python-learning/test.py
+++ Python-learning/test.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 #coding:utf-8
-from random import random
-from math import floor
-firstNums = list(range(1, 34))
-all = [firstNums.pop(floor(random() * len(firstNums))) for tmp in range(6)]
-all.sort()
-all.append(1 + floor(random() * 16))
-print(all)
+
+import unittest
+
+class MyTest(unittest.TestCase):
+  def testd(self):
+    self.assertEqual(0, 0)
+  def t(self):
+      self.assertEqual(0, 2)
+unittest.main()
