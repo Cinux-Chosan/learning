029da3802560cf6ece67bb4337423eeaed158aad
chosan <179817004@qq.com>
2018-06-19 21:29:07 +0800

    ts module

--- Javascript-learning/TypeScript/TypeScript.doc.md/test/greeter.ts
+++ Javascript-learning/TypeScript/TypeScript.doc.md/test/greeter.ts
@@ -1,21 +1,22 @@
 class Greeter {
-  static standardGreeting = "Hello, there";
-  greeting: string;
-  greet() {
-      if (this.greeting) {
-          return "Hello, " + this.greeting;
-      }
-      else {
-          return Greeter.standardGreeting;
-      }
-  }
+    static standardGreeting = "Hello, there";
+    greeting: string;
+    greet() {
+        if (this.greeting) {
+            return "Hello, " + this.greeting;
+        }
+        else {
+            return Greeter.standardGreeting;
+        }
+    }
 }
 
 let greeter1: Greeter;
 greeter1 = new Greeter();
 console.log(greeter1.greet());
 
-let greeterMaker: "function" = Greeter;
+let greeterMaker: typeof Greeter = Greeter;
 greeterMaker.standardGreeting = "Hey there!";
+
 let greeter2: Greeter = new greeterMaker();
 console.log(greeter2.greet());
